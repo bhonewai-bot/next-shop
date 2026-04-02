a26d077c9824b8f33af02e29f82dea990b331813
Bhone Wai <damenaosan@gmail.com>
2026-04-02 20:43:54 +0630

    SSG & ISR

--- app/(root)/blog/authors/[authorId]/page.tsx
+++ app/(root)/blog/authors/[authorId]/page.tsx
@@ -1,10 +1,28 @@
+export const dynamicParams = false;
+
+export const revalidate = 60; // revalidate this page every 60 seconds
+
+export async function generateStaticParams() {
+  /* const authors = await fetch(
+    "https://jsonplaceholder.typicode.com/users",
+  ).then((res) => res.json());
+  return authors.map((author: { id: string }) => ({
+    authorId: author.id,
+  })); */
+  return [{ authorId: "1" }, { authorId: "2" }, { authorId: "3" }];
+}
+
 async function AuthorDetail({
   params,
 }: {
   params: Promise<{ authorId: string }>;
 }) {
   const { authorId } = await params;
-  return <>Author {authorId}</>;
+  return (
+    <div>
+      Author {authorId} {new Date().toLocaleTimeString()}
+    </div>
+  );
 }
 
 export default AuthorDetail;
